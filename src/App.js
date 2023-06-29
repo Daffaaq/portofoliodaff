64edfc9417414c1e588424a1fd9960ee0762bb9b
Daffaaq <daffaaqila48@gmail.com>
2023-06-29 08:31:30 +0700

    updateagain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/navbar';
 import Beranda from './components/beranda';
 import Tentang from './components/tentang';
