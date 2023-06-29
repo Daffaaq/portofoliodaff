b82f689cf6417ed7701ce7bb633bb500b473fcf0
Daffaaq <daffaaqila48@gmail.com>
2023-06-29 07:55:25 +0700

    only navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import './navbar.css'; // Import CSS file
+
+const NavbarComponent = () => {
+    return (
+        <Navbar className="navbar-custom" expand="lg">
+            <Navbar.Brand href="/">Beranda</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="/tentang">Tentang</Nav.Link>
+                    <Nav.Link href="/kontak">Kontak</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default NavbarComponent;
