0378a1ce8948abbee0e5bbcc38bd6b603bd55aab
Daffaaq <daffaaqila48@gmail.com>
2023-07-01 16:10:41 +0700

    app.js sudah clean code and reuse code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Beranda from './components/beranda';
 import Tentang from './components/tentang';
 import Kontak from './components/kontak';
 import Portofolio from './components/portofolio';
 
+const routes = [
+  { path: '/', component: Beranda },
+  { path: '/beranda', component: Beranda },
+  { path: '/tentang', component: Tentang },
+  { path: '/portofolio', component: Portofolio },
+  { path: '/kontak', component: Kontak }
+];
+
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Beranda} />
-        <Route exact path="/beranda" component={Beranda} />
-        <Route path="/tentang" component={Tentang} />
-        <Route path="/portofolio" component={Portofolio} />
-        <Route path="/kontak" component={Kontak} />
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            exact
+            path={route.path}
+            component={route.component}
+          />
+        ))}
       </div>
     </BrowserRouter>
   );
