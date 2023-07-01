28ee2adcb8d70b5eaa8b3c6ffeaf6bf51dd67170
Daffaaq <daffaaqila48@gmail.com>
2023-07-01 15:52:50 +0700

    update_again

--- src/components/beranda.js
+++ src/components/beranda.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Beranda = () => {
     return (
         <div>
             <h1>Selamat datang di halaman Beranda!</h1>
             {/* Tambahkan konten halaman Beranda lainnya di sini */}
+            <Link to="/tentang">
+                <button>Ke Halaman About</button>
+            </Link>
+            <Link to="/portofolio">
+                <button>Ke Halaman portfolio</button>
+            </Link>
+            <Link to="/kontak">
+                <button>Ke Halaman Contact</button>
+            </Link>
         </div>
     );
 };
