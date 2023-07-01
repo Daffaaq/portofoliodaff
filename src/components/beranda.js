237e5172752b14e7102106a1bc7338fdddd07dcd
Daffaaq <daffaaqila48@gmail.com>
2023-07-01 16:15:22 +0700

    update reuse code and clean code

--- src/components/beranda.js
+++ src/components/beranda.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const MenuItem = ({ to, text }) => {
+    return (
+        <Link to={to}>
+            <button>{text}</button>
+        </Link>
+    );
+};
+
+const menuItems = [
+    { to: '/tentang', text: 'Ke Halaman About' },
+    { to: '/portofolio', text: 'Ke Halaman Portfolio' },
+    { to: '/kontak', text: 'Ke Halaman Contact' }
+];
+
 const Beranda = () => {
     return (
         <div>
             <h1>Selamat datang di halaman Beranda!</h1>
             {/* Tambahkan konten halaman Beranda lainnya di sini */}
-            <Link to="/tentang">
-                <button>Ke Halaman About</button>
-            </Link>
-            <Link to="/portofolio">
-                <button>Ke Halaman portfolio</button>
-            </Link>
-            <Link to="/kontak">
-                <button>Ke Halaman Contact</button>
-            </Link>
+            {menuItems.map((menuItem, index) => (
+                <MenuItem key={index} to={menuItem.to} text={menuItem.text} />
+            ))}
         </div>
     );
 };
